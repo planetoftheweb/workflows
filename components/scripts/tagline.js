97251535f131886857c8e051eca6979e08a6d5e7
Ray Villalobos <ray@lynda.com>
2014-05-09 16:59:51 -0400

    Added CoffeeScript files

---
+++ components/scripts/tagline.js
@@ -0,0 +1,7 @@
+var fill;
+
+(fill = function(item) {
+  return $('.tagline').append("" + item);
+})('The most creative minds in Art');
+
+fill;
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-  gutil.log('Workflows are awesome');
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
+gulp.task('coffee', function() {
+  gulp.src(coffeeSources)
+    .pipe(coffee({ bare: true })
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
 });
