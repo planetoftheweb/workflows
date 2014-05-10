8dc7587cb42ba230f2f223e5357068c06f2c8617
Ray Villalobos <ray@lynda.com>
2014-05-10 18:54:02 -0400

    Added Watch task

--- gulpfile.js
+++ gulpfile.js
@@ -39,4 +39,10 @@ gulp.task('compass', function() {
     .pipe(gulp.dest('builds/development/css'))
 });
 
+gulp.task('watch', function() {
+  gulp.watch(coffeeSources, ['coffee']);
+  gulp.watch(jsSources, ['js']);
+  gulp.watch('components/sass/*.scss', ['compass']);
+});
+
 gulp.task('default', ['coffee', 'js', 'compass']);
