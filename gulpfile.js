c7afa669fa1ca57c9ed083dcc359b8c62bf123e3
Ray Villalobos <ray@lynda.com>
2014-05-10 18:56:09 -0400

    added watch to default task

--- gulpfile.js
+++ gulpfile.js
@@ -45,4 +45,4 @@ gulp.task('watch', function() {
   gulp.watch('components/sass/*.scss', ['compass']);
 });
 
-gulp.task('default', ['coffee', 'js', 'compass']);
+gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
